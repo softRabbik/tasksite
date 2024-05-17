7e3ea01ad94102f5e639fa8852ffb4a670932e6f
Sans <kabyshko.stanislav@yandex.ru>
2024-05-17 17:32:47 +0300

    Edit configuration for database management system.

--- philharmonic/philharmonic/settings.py
+++ philharmonic/philharmonic/settings.py
@@ -61,8 +61,15 @@ WSGI_APPLICATION = 'philharmonic.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'PhilharmonicDB',
+        'USER': 'root',
+        'PASSWORD': '128432506',
+        'HOST': '127.0.0.1',
+        'PORT': '3306',
+        'OPTIONS': {
+            'init_command': "SET sql_mode='STRICT_TRANS_TABLES'"
+        }
     }
 }
 
